38df1801e0f1ad2bdfabb708bc5f454913dcc717
Vlad Smelko <VladSmelko@gmail.com>
2021-12-12 14:12:15 +0200

    Added boardgames page

--- bgc/views.py
+++ bgc/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import B_game
 # Create your views here.
 def index(request):
     #homepage
     return render(request, 'bgc/index.html')
+
+def b_games(request):
+    #show all boardgames
+    b_games = B_game.objects.order_by('date_added')
+    context = {'b_games': b_games}
+    return render(request, 'bgc/b_games.html', context)
