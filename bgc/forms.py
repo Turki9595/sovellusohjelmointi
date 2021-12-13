9f20905393db81d70a68d7fed142f211bed91231
Vlad Smelko <VladSmelko@gmail.com>
2021-12-13 13:18:06 +0200

    new review valmis

--- bgc/forms.py
+++ bgc/forms.py
@@ -1,8 +1,15 @@
 from django import forms 
-from .models import B_game
+from .models import B_game,Review
 
 class B_gameForm(forms.ModelForm):
     class Meta:
         model = B_game
         fields = ['name','description']
         labels = {'name': '','description': ''}
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['text','stars']
+        labels = {'text': '','stars': ''}
+        widgets = {'text' : forms.Textarea(attrs={'cols': 80})}
